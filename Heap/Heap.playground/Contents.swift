0f7428b6e0c12aaf8846ebe538ad104188a23992
Alcivanio <alcivaniosilva@gmail.com>
2020-02-15 16:57:36 -0500

    Adding heap structure

---
+++ Heap/Heap.playground/Contents.swift
@@ -0,0 +1,119 @@
+import UIKit
+
+
+//min heap
+class Heap<T: Comparable> {
+    
+    var capacity: Int = 10
+    var count: Int = 0
+    var isEmpty: Bool { return count == 0 }
+    
+    var items: [T?]
+    
+    init() {
+        items = Array(repeating: nil, count: capacity)
+    }
+    
+    private func getParentIndex(_ index: Int) -> Int { return (index - 1) / 2 }
+    private func getLeftChildIndex(_ index: Int) -> Int { return 2 * index + 1 }
+    private func getRightChildIndex(_ index: Int) -> Int { return 2 * index + 2 }
+    
+    private func hasParent(_ index: Int) -> Bool { return getParentIndex(index) >= 0 }
+    private func hasLeftChild(_ index: Int) -> Bool { return getLeftChildIndex(index) < count }
+    private func hasRightChild(_ index: Int) -> Bool { return getRightChildIndex(index) < count }
+    
+    private func parent(_ index: Int) -> T { return items[getParentIndex(index)]! }
+    private func leftChild(_ index: Int) -> T { return items[getLeftChildIndex(index)]! }
+    private func rightChild(_ index: Int) -> T { return items[getRightChildIndex(index)]! }
+    
+    private func ensureExtraCapacity() {
+        if capacity == count {
+            items.append(contentsOf: Array(repeating: nil, count: capacity))
+            capacity = capacity * 2
+        }
+    }
+    
+    public func peek() -> T? {
+        return items[0]
+    }
+    
+    public func poll() -> T? {
+        if count == 0 { return nil }
+        let item = items[0]
+        items[0] = items[count-1]
+        count+=1
+        heapifyDown()
+        return item
+    }
+    
+    public func add(_ item: T) {
+        ensureExtraCapacity()
+        items[count] = item
+        count+=1
+        heapifyUp()
+    }
+    
+    public func printHeap() {
+        for i in 0..<count {
+            print("\(items[i]!)", terminator: " ")
+        }
+        print("")
+    }
+    
+    private func heapifyUp() {
+        if isEmpty { return }
+        var index = count - 1
+        while hasParent(index) && parent(index) > items[index]! {
+            items.swapAt(index, getParentIndex(index))
+            index = getParentIndex(index)
+        }
+    }
+    
+    private func heapifyDown() {
+        if isEmpty { return }
+        var index = 0
+        var smallestChildIndex: Int
+        
+        while hasLeftChild(index) {
+            smallestChildIndex = getLeftChildIndex(index)
+            if hasRightChild(index) && rightChild(index) < leftChild(index) {
+                smallestChildIndex = getRightChildIndex(index)
+            }
+            
+            if items[index]! < items[smallestChildIndex]! { break }
+            
+            items.swapAt(index, smallestChildIndex)
+            index = smallestChildIndex
+        }
+    }
+    
+    
+}
+
+
+
+
+
+let heap = Heap<Int>()
+heap.add(10)
+heap.printHeap()
+heap.add(20)
+heap.printHeap()
+heap.add(30)
+heap.printHeap()
+heap.add(9)
+heap.printHeap()
+heap.add(8)
+heap.printHeap()
+heap.add(32)
+heap.printHeap()
+heap.add(2)
+heap.printHeap()
+
+
+
+
+
+
+
+
